c80b535c0301aaa45bf049c63ec9ca7c1e46fe9a
Clément Guiton <clement.guiton.dev@gmail.com>
2024-03-12 09:03:13 +0100

    remove service test

--- src/main/java/fr/esgi/fx/avis/controller/EditeurController.java
+++ src/main/java/fr/esgi/fx/avis/controller/EditeurController.java
@@ -2,8 +2,11 @@ package fr.esgi.fx.avis.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import fr.esgi.fx.avis.model.Editeur;
 import fr.esgi.fx.avis.usecases.EditeurUseCases;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -22,6 +25,25 @@ public class EditeurController {
         log.info("dans editeurs()");
         ModelAndView mav = new ModelAndView("editeurs");
         mav.addObject("editeurs", editeurUseCases.recupererEditeurs());
+        return mav;
+    }
+
+    /**
+     * Cette méthode traite la requête HTTP qui demande à accéder au formulaire
+     * HTML
+     * d'ajout d'un éditeur
+     *
+     * @return
+     */
+    @GetMapping({ "editeur" })
+    public ModelAndView editeurGet() {
+        ModelAndView mav = new ModelAndView();
+        // On fait appel à la vue (aka template) nommée editeur.html
+        mav.setViewName("editeur");
+        // On injecte un nouvel éditeur sur le formulaire HTML de cette vue
+        // on doit pour ce faire instancier un nouvel objet
+        mav.addObject("editeur", new Editeur());
+
         return mav;
     }
 }
